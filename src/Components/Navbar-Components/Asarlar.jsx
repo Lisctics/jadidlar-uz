57c1ef2cc65dc2c16fbd4b2d95009a2bb223e8cf
Shuxrat <shuhrat.umarov.05@bk.ru>
2024-02-12 22:36:40 +0500

    Ничего нового

--- src/Components/Navbar-Components/Asarlar.jsx
+++ src/Components/Navbar-Components/Asarlar.jsx
@@ -20,21 +20,14 @@ function Asarlar() {
     return(
         <>
             <h1 className="tab_menu_h1 mt-0 pt-20">Asarlar</h1>
-            <div className="div_cont !grid !grid-cols-3 !ml-2 !gap-x-10  lg:!gap-x-10 lg:!grid-cols-4 md:!gap-x-0 md:!grid-cols-4">
-           {/* {apiData?.results.map((item) => (
-                <div className='card-item' key={item.id}>
-                <img className='card-item_img' src={item.image} alt=""/>
-                <p className='card-item_h1'>{item.jadid_fullname}</p>
-                <p className='card-item_h1'>{item.type}</p>
-              </div>
-            ))} */}
+            <div className="div_cont">
             {apiData ? (
                         apiData?.results?.length > 0 ? ( 
                           apiData?.results.map((item) => (
-                            <div className='card-item !h-44 !w-28 lg:!h-477 lg:!w-72' key={item.id}>
-                            <img className='card-item_img !h-36 !w-28 lg:!h-367 lg:!w-72' src={item.image} alt=""/>
-                            <p className='card-item_h1 !text-xs lg:!text-xl lg:pt-5'>{item.jadid_fullname}</p>
-                            <p className='card-item_h1'>{item.type}</p>
+                            <div className='card-item' key={item.id}>
+                              <img className='card-item_img' src={item.image} alt=""/>
+                              <p className='card-item_h1'>{item.jadid_fullname}</p>
+                              <p className='card-item_h1'>{item.type}</p>
                           </div>
                         )
                             )
