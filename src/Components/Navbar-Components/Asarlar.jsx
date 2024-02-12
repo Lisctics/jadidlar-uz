4ed47d74a7af9e0b1748f370042bbd033f17c079
Shuxrat <shuhrat.umarov.05@bk.ru>
2024-02-13 00:00:40 +0500

    deployed

--- src/Components/Navbar-Components/Asarlar.jsx
+++ src/Components/Navbar-Components/Asarlar.jsx
@@ -20,7 +20,7 @@ function Asarlar() {
     return(
         <>
             <h1 className="tab_menu_h1 mt-0 pt-20">Asarlar</h1>
-            <div className="div_cont">
+            <div className="div_cont ">
             {apiData ? (
                         apiData?.results?.length > 0 ? ( 
                           apiData?.results.map((item) => (
